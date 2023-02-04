2eb26d4dbd871fc16f52de3e04e77882e48fde86
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2023-02-04 16:01:20 +0100

    add guard example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AutoLoginPartialRoutesGuard } from 'angular-auth-oidc-client';
+import { AuthorizationGuard } from './auth/authorized.guard';
 import { ChildComponent } from './child/child.component';
 
 const routes: Routes = [
   {
     path: 'child',
     component: ChildComponent,
-    canActivate: [AutoLoginPartialRoutesGuard],
+    canActivate: [AutoLoginPartialRoutesGuard, AuthorizationGuard],
   },
   {
     path: 'lazy',
-    canActivate: [AutoLoginPartialRoutesGuard],
+    canActivate: [AutoLoginPartialRoutesGuard, AuthorizationGuard],
     loadChildren: () => import('./lazy/lazy.module').then((m) => m.LazyModule),
   },
 ];
